9f0a29231ee93439f05e02a7e6a5db6a3266cb97
Mateus Koehler <mateus@mateuskoehler.com>
2019-10-11 14:25:59 -0400

    static build fix

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 //Sub-Routes
-const apiRoutes = require('./api');
-const authRoutes = require('./auth');
+const apiRoutes = require("./api");
+const authRoutes = require("./auth");
 
-router.use('/api', apiRoutes);
-router.use('/auth', authRoutes);
-
-// Catch all React pages
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+router.use("/api", apiRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
